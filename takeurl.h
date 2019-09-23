993007cb66b68806d4ecff3ea085e9f1b621584b
Joshua <josui77@hotmail.com>
2019-09-22 23:58:09 -0600

    Taking image url

---
+++ takeurl.h
@@ -0,0 +1,16 @@
+#ifndef TAKEURL_H
+#define TAKEURL_H
+
+#include <QString>
+class takeurl
+{
+public:
+    takeurl();
+    ~takeurl();
+
+    void Take(QString element);
+    QString SendImage(QString inputurl );
+
+};
+
+#endif // TAKEURL_H
